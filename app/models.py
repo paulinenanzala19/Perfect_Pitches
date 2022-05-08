23c588e7ae01535767b95aa56ee96e1e05e3bd88
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-08 15:28:13 +0300

    adding class pitches

---
+++ app/models.py
@@ -0,0 +1,16 @@
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    pitch_title = db.Column(db.String)
+    pitch_content = db.Column(db.String(1000))
+    category = db.Column(db.String)
+    
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_pitches(cls,category):
+        pitches = Pitch.query.filter_by(category=category).all()
+        return pitches
