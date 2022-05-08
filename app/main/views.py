a67c0939b7c4169fc567182a3d58c854efb625ca
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-08 16:27:34 +0300

    adding a macros and an index file

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,23 @@
 from flask import render_template
 from . import main
+from ..models import Pitch
 
 @main.route('/')
 def index():
 
+    pickuplines_pitches=Pitch.get_pitches('pickuplines')
+    idea_pitches=Pitch.get_pitches('idea')
+    music_pitches=Pitch.get_pitches('music')
+    business_pitches=Pitch.get_pitches('business')
 
-    return render_template('index.html')
+
+    return render_template('index.html',pickuplines=pickuplines_pitches, 
+    idea=idea_pitches,music=music_pitches,business=business_pitches)
+
+@main.route('/pitches/business_pitches')
+def business_pitches():
+
+    pitches = Pitch.get_pitches('business')
+
+    return render_template("business_pitches.html", pitches = pitches)
     
--- manage.py
+++ manage.py
@@ -1,10 +1,15 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager, Server
+from app.models import Pitch
 
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,Pitch = Pitch )
 
 
 if __name__=='__main__':
