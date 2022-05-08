10322c41ad0214b02e1cb43c2edb39b7238ea3b2
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-08 19:37:51 +0300

    adding the navbar

--- app/main/views.py
+++ app/main/views.py
@@ -5,13 +5,15 @@ from ..models import Pitch
 @main.route('/')
 def index():
 
+    title='Perfect Pitch'
+
     pickuplines_pitches=Pitch.get_pitches('pickuplines')
     idea_pitches=Pitch.get_pitches('idea')
     music_pitches=Pitch.get_pitches('music')
     business_pitches=Pitch.get_pitches('business')
 
 
-    return render_template('index.html',pickuplines=pickuplines_pitches, 
+    return render_template('index.html',title=title,pickuplines=pickuplines_pitches, 
     idea=idea_pitches,music=music_pitches,business=business_pitches)
 
 @main.route('/pitches/business_pitches')
