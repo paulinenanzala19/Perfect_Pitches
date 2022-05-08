7779b4aef09e07f6c3b6e478be98a2a28b98bfe7
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-09 01:03:10 +0300

    adding wtf forms

---
+++ app/main/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import InputRequired
+
+class ReviewForm(FlaskForm):
+
+    title = StringField('Review title',validators=[InputRequired()])
+    text = TextAreaField('Pitch review', validators=[InputRequired()])
+    category=selectField('category' select=[('pickuplines','pickup -lines_pitches'),('idea','Idea_pitches'),('music','Music_pitches'),('business','Business_Pitches')])
+    submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [InputRequired()])
+    submit = SubmitField('Submit')
