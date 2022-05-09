0c7c67441f79b3b23205bb313dc5d96c0b89189e
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-10 01:15:46 +0300

    creating an auth folder to authorize users credentials

--- app/models.py
+++ app/models.py
@@ -1,5 +1,23 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
+
+class User(db.Model):
+
+    pass_secure  = db.Column(db.String(255))
+
+        @property
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+        @password.setter
+        def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+
 
 class Pitch(db.Model):
     __tablename__ = 'pitches'
