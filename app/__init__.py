588a68cad573045eed9cfe255cd8f50fc01bc4b7
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-10 01:21:53 +0300

    registering our blueprint instance in create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,9 @@ bootstrap= Bootstrap()
 def create_app(config_name):
     app=Flask(__name__)
 
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
     app.config.from_object(config_options[config_name])
     db.init_app(app)
     bootstrap.init_app(app)
