48508421e953177a82bcecdbc74334b50135c5eb
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-10 01:32:57 +0300

    creating an instance of the class Login Manager

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,10 @@ from flask_login import LoginManager
 
 db=SQLAlchemy()
 bootstrap= Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 def create_app(config_name):
     app=Flask(__name__)
@@ -16,6 +20,7 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
     db.init_app(app)
     bootstrap.init_app(app)
+    login_manager.init_app(app)
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
