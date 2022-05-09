fd627a63e77d9929a0d9ac42f69586669eccd6e8
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-10 01:25:08 +0300

    installing a flask log in extension that allows us to manage the user authentication system

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db=SQLAlchemy()
 bootstrap= Bootstrap()
