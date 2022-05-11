f0008f6ad2f0bfecc630bd2b2c161bc649076b11
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-11 14:29:39 +0300

    feature:adding the update.html  in our profile

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -23,3 +23,4 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators =[InputRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
+
