8295cc51cf4c75805836f9332dfb79299ae5517e
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-12 20:56:17 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -1,16 +1,17 @@
 from app import create_app,db
 from flask_script import Manager, Server
 from app.models import Pitch
-from  flask_migrate import Migrate
+from  flask_migrate import Migrate,MigrateCommand
 
 
 app = create_app('production')
 
-
-
-manager = Manager(app)
-manager.add_command('server',Server)
 migrate = Migrate(app,db)
+manager = Manager(app)
+
+manager.add_command('server',Server)
+manager.add_command('db',MigrateCommand)
+
 @manager.command
 def test():
     """Run the unit tests."""
