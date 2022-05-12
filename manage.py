e71e408097b0d25e55cefbd1f684020200624783
paulinenanzala19 <pauline.wafula@student.moringaschool.c0m>
2022-05-12 21:07:03 +0300

    deployment to heroku

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 from app import create_app,db
 from flask_script import Manager, Server
 from app.models import Pitch
-from  flask_migrate import Migrate
+from  flask_migrate import Migrate,MigrateCommand
 
 
 app = create_app('production')
@@ -10,7 +10,7 @@ migrate = Migrate(app,db)
 manager = Manager(app)
 
 manager.add_command('server',Server)
-manager.add_command('db')
+manager.add_command('db'MigrateCommand)
 
 @manager.command
 def test():
